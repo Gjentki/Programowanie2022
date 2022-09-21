5a1ba2e46dfa6a120ea9fc666f938d02e47bc2c1
Gjentki <85639883+Gjentki@users.noreply.github.com>
2022-09-21 08:25:05 +0200

    Delete Arkusz_E.14_2016_październik-ZAD.01_PHP directory

--- Arkusz_E.14_2016_październik-ZAD.01_PHP/prawy.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $connect = mysqli_connect('127.0.0.1', 'root', '', 'szkola');
-    $prawy = mysqli_query($connect,"SELECT imie, nazwisko FROM uczen WHERE id=2");
-    while($row = mysqli_fetch_array($prawy)){
-        echo $row['imie'] . " ". $row['nazwisko'];
-    }
-    
-?>
